94b65d87af99479bacabc31bc34f5e3ef15b6807
Buqra <buqraoner@gmail.com>
2022-09-13 02:19:35 +0300

    logo && screens

--- app/config/styles.js
+++ app/config/styles.js
@@ -10,4 +10,15 @@ export default {
         fontSize: 18,
         fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     },
+    screens: {
+        padding: 10,
+        backgroundColor: colors.light
+    },
+    logo: {
+        width: 80,
+        height: 80,
+        alignSelf: "center",
+        marginTop: 50,
+        marginBottom: 25
+    }
 };
