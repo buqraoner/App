1c2ef7c1cd5edefa1ee51923c495ff656fb2592f
Buqra <boletroy@gmail.com>
2022-09-20 17:20:02 +0300

    .

--- App.js
+++ App.js
@@ -1,44 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import React, { useState, useEffect } from 'react';
-import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
-
-
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ListItem from './app/components/lists/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import AppFormPicker from './app/components/forms/AppFormPicker';
-import RegisterScreen from './app/screens/RegisterScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
-import ImageInput from './app/components/ImageInput';
-import ImageInputList from './app/components/ImageInputList';
-
-const Stack = createNativeStackNavigator();
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState();
-
   return (
-    <Screen>
-      <ListingEditScreen />
-    </Screen>
-
-  )
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
 
 
