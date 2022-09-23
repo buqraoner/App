051947a12b22096f83a0dc4b0f52fd25336f1d34
Buqra <boletroy@gmail.com>
2022-09-23 22:28:10 +0300

    flashlits

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,51 +1,70 @@
-import React from 'react'
-import { StyleSheet, FlatList, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet, ScrollView } from 'react-native'
+import { FlashList } from '@shopify/flash-list'
+
 import routes from '../navigation/routes'
-
-
+import useApi from '../hooks/useApi'
 import colors from '../config/colors'
-
+import listingsApi from '../api/listings'
 import Screen from '../components/Screen'
+import AppText from '../components/AppText'
 import Card from '../components/Card'
+import AppButton from '../components/AppButton'
+import ActivityIndicator from '../components/ActivityIndicator'
 
-const listing = [
-    {
-        id: 1,
-        title: "Red jacket for sale",
-        price: 100,
-        image: require("../assets/jacket.jpg"),
-    },
-    {
-        id: 2,
-        title: "Couch in great condition",
-        price: 1000,
-        image: require("../assets/couch.jpg"),
-    },
-]
+
 
 export default function ListingsScreen({ navigation }) {
+    const getListingsApi = useApi(listingsApi.getListings);
+    useEffect(() => {
+        getListingsApi.request();
+
+    }, []);
+
     return (
-        <Screen style={styles.container}>
-            <FlatList
-                data={listing}
-                keyExtractor={listing => listing.id.toString()}
-                renderItem={({ item }) =>
-                    <Card
-                        title={item.title}
-                        subTitle={"$" + item.price}
-                        image={item.image}
-                        onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-                    />
-                }
-            />
-        </Screen>
+        <ScrollView>
+            <Screen style={styles.screen}>
+                {getListingsApi.error && (
+                    <>
+                        <AppText style={styles.error}>
+                            Couldn't retrieve the listings.
+                        </AppText>
+                        <AppButton title="Retry" onPress={getListingsApi.request} />
+                    </>)}
+                <ActivityIndicator
+                    visible={getListingsApi.loading}
+                    size="large"
+                    color={colors.primary}
+                />
+                <FlashList
+                    data={getListingsApi.data}
+                    keyExtractor={(listing) => listing.id.toString()}
+                    estimatedItemSize={250}
+                    renderItem={({ item }) => (
+                        <Card
+                            title={item.title}
+                            subTitle={"$" + item.price}
+                            imageUrl={item.images[0].url}
+                            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+                        />
+                    )}
+                />
+            </Screen>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    screen: {
         padding: 20,
         backgroundColor: colors.lightorange,
+    },
+    error: {
+        color: colors.danger,
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical: 20,
     }
 })
 
